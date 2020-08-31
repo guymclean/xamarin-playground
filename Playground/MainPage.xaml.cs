00cafbd933d0db99b66915d0c2bf6b69a6e95339
Guy McLean <guymclean@Guys-MacBook-Pro.local>
2020-08-31 15:15:21 +0100

    Initial commit

---
+++ Playground/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Playground
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title here", "this is the message", "OK");
+        }
+    }
+}
